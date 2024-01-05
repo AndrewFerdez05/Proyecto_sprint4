a6bdc06bdbdc0ee36aa8e4b6e0b4b976665cc911
Andrew Ferdez <andrew.ferdez1607@gamil.com>
2024-01-05 18:49:50 -0500

    Se crea casilla de verificación

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-        
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-disp_button = st.button('Construir diagrama de dispersión') # crear un botón
 
 st.header('Data de venta de vehículos')
         
-if hist_button: # al hacer clic en el botón
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+build_histogram = st.button('Construir histograma') # crear un botón
+disp_button = st.button('Construir diagrama de dispersión') # crear un botón
+
+        
+if build_histogram: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
