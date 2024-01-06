6f4fab41ed6dea5b25b6cbb79fe180e44d5a6692
Andrew Ferdez <andrew.ferdez1607@gamil.com>
2024-01-05 19:21:38 -0500

    update checkbox

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ st.header('Data de anuncio de ventas de vehículos')
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 build_histogram = st.checkbox('Construir histograma') # crear un botón
-build_disp = st.checkbox('Construir diagrama de dispersión') # crear un botón
+build_dispersion = st.checkbox('Construir diagrama de dispersión') # crear un botón
 
         
 if build_histogram: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para la columna odómetro')
             
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
@@ -19,9 +19,9 @@ if build_histogram: # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-if build_disp: # al hacer clic en el botón
+if build_dispersion: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un diagrama de dispersión para la columna odómetro y precio')
             
     # crear un histograma
     fig2 = px.scatter(car_data, x="odometer", y="price")
