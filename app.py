05a2c8c23576af9ebdd892a8680ba47a190769c3
Andrew Ferdez <andrew.ferdez1607@gamil.com>
2024-01-05 19:04:06 -0500

    update

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header('Data de venta de vehículos')
+st.header('Data de anuncio de ventas de vehículos')
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-build_histogram = st.button('Construir histograma') # crear un botón
-disp_button = st.button('Construir diagrama de dispersión') # crear un botón
+build_histogram = st.checkbox('Construir histograma') # crear un botón
+build_disp = st.checkbox('Construir diagrama de dispersión') # crear un botón
 
         
 if build_histogram: # al hacer clic en el botón
@@ -19,7 +19,7 @@ if build_histogram: # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-if disp_button: # al hacer clic en el botón
+if build_disp: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
             
